16dad80c77c05e2a3bc33a4381668312d72ac6a6
kumargy-pythondev <kumargy.pythondev@gmail.com>
2021-03-16 19:35:37 -0400

    practice again

--- myDjangoProject/first_app/apps.py
+++ myDjangoProject/first_app/apps.py
@@ -5,3 +5,4 @@ class FirstAppConfig(AppConfig):
     name = 'first_app'
 #the above line is just a variable for a new app created 'first_app'
 # you need not worry about this so much. it is fine
+# again I am adding this for additional practice
