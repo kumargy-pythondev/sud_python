3e6295d021719e2f9854ddb419d021d46fe0a321
Sudeer Kumar <sudeerkumargy@gmail.com>
2021-03-16 19:21:28 -0400

    Update apps.py

--- myDjangoProject/first_app/apps.py
+++ myDjangoProject/first_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FirstAppConfig(AppConfig):
     name = 'first_app'
 #the above line is just a variable for a new app created 'first_app'
+# you need not worry about this so much. it is fine
